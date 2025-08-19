3f989037dda3e55e89420b7ea55e13db0939c7df
Japoncio3k <gustavo.kamihara@gmail.com>
2025-08-19 17:09:37 -0300

    add prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = [
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -12,7 +12,7 @@ const typeDefs = `#graphql
 
 const resolvers = {
   Query: {
-    hello: () => "Hello World!",
+    hello: () => 'Hello World!',
   },
 };
 
