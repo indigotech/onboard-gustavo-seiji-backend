b5cf6623e757719e1c40c94fbe1d4dc13e1dbe7d
Japoncio3k <gustavo.kamihara@gmail.com>
2025-08-20 11:35:26 -0300

    add biome for formatting

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import fastify from 'fastify'
+import fastify from "fastify";
 
-const server = fastify()
+const server = fastify();
 
-server.get('/hello', async (request, reply) => {
-  return 'Hello World!'
-})
+server.get("/hello", async (request, reply) => {
+	return "Hello World!";
+});
 
 server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-})
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log(`Server listening at ${address}`);
+});
