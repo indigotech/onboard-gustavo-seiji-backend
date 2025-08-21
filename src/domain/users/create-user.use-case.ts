76684eb184a81cf158bf26f71885a7a807b979fa
Japoncio3k <gustavo.kamihara@gmail.com>
2025-08-21 11:48:35 -0300

    add hash to env

--- src/domain/users/create-user.use-case.ts
+++ src/domain/users/create-user.use-case.ts
@@ -1,24 +1,17 @@
-import { UserDatasource, UserDbDatasource } from '@data/users/user.db.datasource.js';
-import type { User, UserData, UserInput } from '@models/users.model.js';
-import type { User } from '@prisma/client';
+import { UserDbDatasource } from '@data/users/user.db.datasource.js';
+import type { User, UserInput } from '@models/users.model.js';
 import { hash } from 'bcrypt';
 
 const LETTER_REGEX = /[a-z]/i;
 
-const DIGIT_REGEX = /\d/g;
+const DIGIT_REGEX = /\d/;
 
 const EMAIL_REGEX = /([@][a-z]+.com)/i;
 
-const hashPassword = async (password: string): Promise<string> => {
-  const salt = process.env.ENCRYPTION_SALT;
-
-  return hash(password, salt);
-};
-
-export const createUserUseCase = async (data: UserData): Promise<User> => {
+export const createUserUseCase = async (data: UserInput): Promise<User> => {
   validateFields(data);
-  
-  const user = await UserDatasource.findUserByEmail(data.email);
+
+  const user = await UserDbDatasource.findByEmail(data.email);
 
   if (user) {
     throw new Error('User already exists');
@@ -29,12 +22,22 @@ export const createUserUseCase = async (data: UserData): Promise<User> => {
   return UserDbDatasource.create(data);
 };
 
+const hashPassword = async (password: string): Promise<string> => {
+  const salt = process.env.ENCRYPTION_SALT;
+
+  return hash(password, salt);
+};
+
 const validateFields = (data: UserInput): void => {
   if (data.password.length < 6) {
     throw new Error('Password must be at least 6 characters long');
   }
 
   if (!LETTER_REGEX.test(data.password) || !DIGIT_REGEX.test(data.password)) {
+    console.log(data.password);
+    console.log(!LETTER_REGEX.test(data.password));
+    console.log(!DIGIT_REGEX.test(data.password));
+    console.log(!LETTER_REGEX.test(data.password) || !DIGIT_REGEX.test(data.password));
     throw new Error('Password must contain at least one letter and one digit');
   }
 
@@ -50,3 +53,4 @@ const validateFields = (data: UserInput): void => {
   if (formattedBirthdate > new Date()) {
     throw new Error('Birthdate must be in the past');
   }
+};
