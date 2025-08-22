40f9de24a46585775618d19c22fbb6a3a934eb52
Japoncio3k <gustavo.kamihara@gmail.com>
2025-08-22 03:24:56 -0300

    make api schemas extend domain models

--- src/api/auth/auth.schema.ts
+++ src/api/auth/auth.schema.ts
@@ -1,11 +1,5 @@
-import type { User } from '@models/users.model.js';
+import type { Auth, AuthInput } from '@models/auth.model.js';
 
-export interface AuthRequestBody {
-  email: string;
-  password: string;
-}
+export interface AuthRequestBody extends AuthInput {}
 
-export interface AuthResponse {
-  user: User;
-  token: string;
-}
+export interface AuthResponse extends Auth {}
