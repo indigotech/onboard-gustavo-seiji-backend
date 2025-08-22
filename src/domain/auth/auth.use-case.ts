7d1ad2b561768896099c2177df563d9bee73cc68
Japoncio3k <gustavo.kamihara@gmail.com>
2025-08-22 03:34:35 -0300

    use user datasource instead of creating a new one

--- src/domain/auth/auth.use-case.ts
+++ src/domain/auth/auth.use-case.ts
@@ -1,9 +1,18 @@
 import { hash } from '@core/encryption/hash.js';
-import { authDbDatasource } from '@data/auth/auth.db.datasource.js';
-import type { Auth, AuthInput } from '@models/auth.model.js';
+import { UserDbDatasource } from '@data/users/user.db.datasource.js';
+import { type Auth, AuthErrors, type AuthInput } from '@models/auth.model.js';
 
 export const authUseCase = async (data: AuthInput): Promise<Auth> => {
+  const user = await UserDbDatasource.findByEmail(data.email);
+
   data.password = await hash(data.password);
 
-  return authDbDatasource(data);
+  if (!user || user.password !== data.password) {
+    throw AuthErrors.INVALID_CREDENTIALS;
+  }
+
+  return {
+    user,
+    token: 'the_token',
+  };
 };
