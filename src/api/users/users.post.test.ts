e973eb9eb7a5497940c15b0af8bcb5055d6a6514
Japoncio3k <gustavo.kamihara@gmail.com>
2025-08-22 16:46:02 -0300

    add util function to make request

--- src/api/users/users.post.test.ts
+++ src/api/users/users.post.test.ts
@@ -1,5 +1,6 @@
 import { after, describe, it } from 'node:test';
 import { prisma } from '@core/db/db.js';
+import type { UserInput } from '@models/users.model.js';
 import axios from 'axios';
 import { expect } from 'chai';
 
@@ -10,9 +11,15 @@ const USER_TO_CREATE = {
   birthDate: '1990-01-01T00:00:00.000Z',
 };
 
+const sendTestRequest = async (data: UserInput) => {
+  return axios.post('http://localhost:8080/users', data, {
+    validateStatus: () => true,
+  });
+};
+
 describe('User Creation', () => {
   it('should create a user', async () => {
-    const response = await axios.post('http://localhost:8080/users', USER_TO_CREATE);
+    const response = await sendTestRequest(USER_TO_CREATE);
 
     expect(response.status).to.equal(201);
     expect(response.data).to.have.property('id');
@@ -45,14 +52,10 @@ describe('User Creation', () => {
 
 describe('User Creation Errors', () => {
   it('should return invalid email error', async () => {
-    const response = await axios.post(
-      'http://localhost:8080/users',
-      {
-        ...USER_TO_CREATE,
-        email: 'invalid-email',
-      },
-      { validateStatus: (status: number) => status === 422 },
-    );
+    const response = await sendTestRequest({
+      ...USER_TO_CREATE,
+      email: 'invalid-email',
+    });
 
     expect(response.status).to.equal(422);
 
@@ -64,14 +67,10 @@ describe('User Creation Errors', () => {
   });
 
   it('should return invalid password error', async () => {
-    const responseShortPassword = await axios.post(
-      'http://localhost:8080/users',
-      {
-        ...USER_TO_CREATE,
-        password: 'short',
-      },
-      { validateStatus: (status: number) => status === 422 },
-    );
+    const responseShortPassword = await sendTestRequest({
+      ...USER_TO_CREATE,
+      password: 'short',
+    });
 
     expect(responseShortPassword.status).to.equal(422);
     expect(responseShortPassword.data).to.deep.equal({
@@ -80,14 +79,10 @@ describe('User Creation Errors', () => {
       details: 'Password must contain at least one letter and one digit, and at least 6 characters long.',
     });
 
-    const responseNumberPassword = await axios.post(
-      'http://localhost:8080/users',
-      {
-        ...USER_TO_CREATE,
-        password: '123456',
-      },
-      { validateStatus: (status: number) => status === 422 },
-    );
+    const responseNumberPassword = await sendTestRequest({
+      ...USER_TO_CREATE,
+      password: '123456',
+    });
 
     expect(responseNumberPassword.status).to.equal(422);
     expect(responseNumberPassword.data).to.deep.equal({
@@ -96,14 +91,10 @@ describe('User Creation Errors', () => {
       details: 'Password must contain at least one letter and one digit, and at least 6 characters long.',
     });
 
-    const response = await axios.post(
-      'http://localhost:8080/users',
-      {
-        ...USER_TO_CREATE,
-        password: 'password',
-      },
-      { validateStatus: (status: number) => status === 422 },
-    );
+    const response = await sendTestRequest({
+      ...USER_TO_CREATE,
+      password: 'password',
+    });
 
     expect(response.status).to.equal(422);
     expect(response.data).to.deep.equal({
@@ -114,14 +105,10 @@ describe('User Creation Errors', () => {
   });
 
   it('should return invalid birthdate error', async () => {
-    const responseInvalidDate = await axios.post(
-      'http://localhost:8080/users',
-      {
-        ...USER_TO_CREATE,
-        birthDate: 'invalid-date',
-      },
-      { validateStatus: (status: number) => status === 422 },
-    );
+    const responseInvalidDate = await sendTestRequest({
+      ...USER_TO_CREATE,
+      birthDate: 'invalid-date',
+    });
 
     expect(responseInvalidDate.status).to.equal(422);
     expect(responseInvalidDate.data).to.deep.equal({
@@ -133,14 +120,10 @@ describe('User Creation Errors', () => {
     const futureDate = new Date();
     futureDate.setFullYear(futureDate.getFullYear() + 1);
 
-    const responseFutureDate = await axios.post(
-      'http://localhost:8080/users',
-      {
-        ...USER_TO_CREATE,
-        birthDate: futureDate.toISOString(),
-      },
-      { validateStatus: (status: number) => status === 422 },
-    );
+    const responseFutureDate = await sendTestRequest({
+      ...USER_TO_CREATE,
+      birthDate: futureDate.toISOString(),
+    });
 
     expect(responseFutureDate.status).to.equal(422);
     expect(responseFutureDate.data).to.deep.equal({
@@ -151,11 +134,9 @@ describe('User Creation Errors', () => {
   });
 
   it('should return user already exists error', async () => {
-    await axios.post('http://localhost:8080/users', USER_TO_CREATE);
+    await sendTestRequest(USER_TO_CREATE);
 
-    const response = await axios.post('http://localhost:8080/users', USER_TO_CREATE, {
-      validateStatus: (status: number) => status === 409,
-    });
+    const response = await sendTestRequest(USER_TO_CREATE);
 
     expect(response.status).to.equal(409);
     expect(response.data).to.deep.equal({
